d76d11e2485f8850ad4f5d70c42fe0a106cb40ec
hyunju@Julie-Kim <hyunju.kim520@gmail.com>
2019-09-16 09:58:07 +1000

    End of Section 8

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
+import ScrollTop from "./app/layout/ScrollTop";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <ScrollTop>
+      <App />
+    </ScrollTop>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
