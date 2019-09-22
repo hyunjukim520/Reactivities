873273e8ba0f6db3275d0a1aa2afae6b5f2c5918
hyunju@Julie-Kim <hyunju.kim520@gmail.com>
2019-09-23 09:26:11 +1000

    End of Section 16

--- Application/Activities/MappingProfile.cs
+++ Application/Activities/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using Domain;
 
@@ -10,7 +11,8 @@ namespace Application.Activities
       CreateMap<Activity, ActivityDto>();
       CreateMap<UserActivity, AttendeeDto>()
         .ForMember(destination => destination.Username, option => option.MapFrom(source => source.AppUser.UserName))
-        .ForMember(destination => destination.DisplayName, option => option.MapFrom(source => source.AppUser.DisplayName));
+        .ForMember(destination => destination.DisplayName, option => option.MapFrom(source => source.AppUser.DisplayName))
+        .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
     }
   }
 }
--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -7,6 +7,9 @@ namespace Domain
   {
     public string DisplayName { get; set; }
 
+    public string Bio { get; set; }
+
     public virtual ICollection<UserActivity> UserActivities { get; set; } // virtual matching to opt.UseLazyLoadingProxies();
+    public virtual ICollection<Photo> Photos { get; set; }
   }
 }
